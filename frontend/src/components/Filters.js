9a94d6c30beb744d3dc0ab88c0005d48d072941e
Zefarak <lirageika@hotmail.gr>
2018-10-22 10:49:41 +0300

    dsaff

--- frontend/src/components/Filters.js
+++ frontend/src/components/Filters.js
@@ -4,16 +4,25 @@ import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Filters extends React.Component {
 
+    onChangeCheckbox = (event) =>{
+        event.preventDefault();
+        const id = event.target.value;
+        this.props.filterProducts(id)
+    };
+
     render() {
         const categories = this.props.categories;
-
+        console.log('props cate', categories);
         return (
             <Form>
                 <h4>Filters</h4>
                 {categories.map((category, index)=>(
                     <FormGroup check>
                     <Label check>
-                        <Input type="checkbox" />{' '}
+                        <Input onClick={this.onChangeCheckbox}
+                               type="checkbox"
+                               value={category.id} />
+                        {' '}
                          {category.title}
                     </Label>
                 </FormGroup>
--- frontend/src/views/Order.js
+++ frontend/src/views/Order.js
@@ -5,7 +5,7 @@ import {Container, Row, Col, Card, CardHeader,
 import MyNavbar from '../components/Navbar.js';
 import ProductTable from '../components/ProductTable.js'
 import OrderDetail from '../components/OrderDetails.js'
-
+import Filters from '../components/Filters.js'
 
 class Order extends React.Component{
 
@@ -13,9 +13,29 @@ class Order extends React.Component{
         super(props);
         this.state = {
             products: [],
+            categories: [],
+            selected_categories: [],
             order_data: undefined,
             doneLoading: false
         }
+    }
+
+    getCategories(){
+        const endpoint = 'http://127.0.0.1:8000/api/category-list/';
+        const thisComp = this;
+        let lookupOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        fetch(endpoint, lookupOptions).then((resp)=>resp.json()).then(
+            (responseData) => {
+                thisComp.setState({
+                    categories:responseData
+                })
+            }
+        )
     }
 
     getProducts(){
@@ -42,7 +62,6 @@ class Order extends React.Component{
 
     getOrder(){
         const endpoint = `http://127.0.0.1:8000/api/order-detail/1/`;
-        console.log(endpoint)
         const thisComp = this;
         let lookupOptions = {
             method: 'GET',
@@ -64,8 +83,23 @@ class Order extends React.Component{
 
     }
 
+    filterProducts = (id) => {
+        let categories = this.state.selected_categories;
+        if (categories.indexOf(id)> -1 ){
+            this.setState({
+                selected_categories: this.state.selected_categories.filter((i) => i !== id)
+            })
+        } else{
+            this.setState({
+                selected_categories: categories.concat(id)
+            })
+        }
+
+    };
+
     componentDidMount(){
         const {id} = 1;
+        this.getCategories();
         this.getProducts();
         this.getOrder(id)
     }
@@ -76,16 +110,25 @@ class Order extends React.Component{
             <div>
                 <MyNavbar />
                 <Container>
-                    <Row>
-                        {this.state.doneLoading ?
-                            <div>
-                                <Col xs={8}><ProductPart products={this.state.products} /></Col>
-                                <Col xs={4}><OrderPart order_data={this.state.order_data} /></Col>
-                            </div>
-                            :
-                            <Col xs={12}><p>No data</p></Col>
-                        }
-                    </Row>
+                    {this.state.doneLoading ?
+                        <Row>
+                            <Col xs="8">
+                                <h4 className='header'>Products</h4>
+                                <ProductPart products={this.state.products} />
+                            </Col>
+                            <Col xs="4">
+                                <h4 className='header'> Order Details </h4>
+                                <Filters categories={this.state.categories}
+                                         filterProducts={this.filterProducts}
+                                />
+                                <br />
+                                <OrderPart order_data={this.state.order_data} />
+                            </Col>
+                        </Row>
+                        :
+                        <Col xs={12}><p>No data</p></Col>
+                    }
+
                 </Container>
             </div>
         )
@@ -93,9 +136,7 @@ class Order extends React.Component{
 
 }
 
-
 class ProductPart extends React.Component {
-
 
     render() {
         const products = this.props.products;
@@ -111,19 +152,17 @@ class OrderPart extends React.Component{
         const { order_data} = this.props;
 
         return(
-            <div>
-                <Card>
-                    <CardHeader>Table {order_data.tag_table}</CardHeader>
-                    <div>
+            <Card>
+                <CardHeader>Table {order_data.tag_table}</CardHeader>
+
                         <CardTitle>
                             Notes.. {order_data.title}
                         </CardTitle>
                         <CardText>Value.. {order_data.tag_value}</CardText>
                         <button>{order_data.tag_status}</button>
-                    </div>
-                    <OrderDetail order_data={order_data} />
-                </Card>
-            </div>
+
+                <OrderDetail order_data={order_data} />
+            </Card>
         )
     }
 }
