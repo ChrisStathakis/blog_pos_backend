cca98f475e03eeef872c13ac594af295e8f6506e
Zefarak <lirageika@hotmail.gr>
2018-11-25 12:34:52 +0200

    dsad

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn blog_pos.wsgi --log-file -
--- blog_pos/wsgi.py
+++ blog_pos/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for blog_pos project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_pos.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
